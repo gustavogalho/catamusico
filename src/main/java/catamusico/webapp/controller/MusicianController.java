0088f0472f36d0d48dbbf77d6ab70c3929b5ef1d
Andressa Job Krautler <ajobk@motorola.com>
2023-05-30 10:20:23 -0300

    fix: post method

--- src/main/java/catamusico/webapp/controller/MusicianController.java
+++ src/main/java/catamusico/webapp/controller/MusicianController.java
@@ -1,22 +1,17 @@
 package catamusico.webapp.controller;
 
 
-import java.io.Console;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import catamusico.webapp.domain.Musician;
-import catamusico.webapp.repository.MusicianRepository;
 import catamusico.webapp.service.MusicianService;
 
 @RestController
@@ -24,7 +19,6 @@ import catamusico.webapp.service.MusicianService;
 public class MusicianController {
     @Autowired
     private MusicianService musicianService;
-    private MusicianRepository musicianRepository;
 
     @GetMapping("/")
     public List<Musician> getAll(Model model)
@@ -37,6 +31,6 @@ public class MusicianController {
     public Musician createMusician(@RequestBody Musician newMusician)
     {
         System.out.println(newMusician);
-        return musicianRepository.save(newMusician);
+		return musicianService.createMusician(newMusician);
     }
 }
--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -9,37 +9,35 @@
 -- MUSICIAN
 INSERT INTO musician (id, city, contact, experience_level, experiences, instrument, music_genre, name, state)
 VALUES
-(1, 'Nova Iorque', 'john@example.com', 'Intermediário', 'Tocou em uma banda local por 3 anos', 'Guitarra', 'Rock', 'John Smith', 'Nova York'),
-(2, 'Los Angeles', 'sarah@example.com', 'Avançado', 'Fez turnês com um artista famoso de pop', 'Piano', 'Pop', 'Sarah Johnson', 'Califórnia'),
-(3, 'Londres', 'mike@example.com', 'Iniciante', 'Apresentou em pequenos locais locais', 'Bateria', 'Indie', 'Mike Anderson', 'Inglaterra'),
-(4, 'Berlim', 'anna@example.com', 'Intermediário', 'Gravou em estúdio para um álbum de jazz', 'Saxofone', 'Jazz', 'Anna Müller', 'Alemanha'),
-(5, 'Tóquio', 'takashi@example.com', 'Avançado', 'Tocou em uma orquestra por 10 anos', 'Violino', 'Clássico', 'Takashi Yamamoto', 'Japão'),
-(6, 'Paris', 'emilie@example.com', 'Intermediário', 'Participou de vários festivais de jazz', 'Baixo', 'Jazz', 'Jovinalda Dupont', 'França'),
-(7, 'Sydney', 'liam@example.com', 'Iniciante', 'Apresentou em bares e pubs locais', 'Vocal', 'Pop/Rock', 'Liam Evans', 'Austrália'),
-(8, 'Toronto', 'emily@example.com', 'Avançado', 'Fez turnês com uma banda country', 'Banjo', 'Country', 'Emily Thompson', 'Canadá'),
-(9, 'Moscou', 'vladimir@example.com', 'Intermediário', 'Tocou em uma orquestra sinfônica', 'Violoncelo', 'Clássico', 'Vladimir Petrov', 'Rússia'),
-(10, 'São Paulo', 'maria@example.com', 'Iniciante', 'Participou de apresentações de talentos escolares', 'Flauta', 'Pop', 'Maria Silva', 'Brasil');
+(1001, 'Nova Iorque', 'john@example.com', 'Intermediário', 'Tocou em uma banda local por 3 anos', 'Guitarra', 'Rock', 'John Smith', 'Nova York'),
+(1002, 'Los Angeles', 'sarah@example.com', 'Avançado', 'Fez turnês com um artista famoso de pop', 'Piano', 'Pop', 'Sarah Johnson', 'Califórnia'),
+(1003, 'Londres', 'mike@example.com', 'Iniciante', 'Apresentou em pequenos locais locais', 'Bateria', 'Indie', 'Mike Anderson', 'Inglaterra'),
+(1004, 'Berlim', 'anna@example.com', 'Intermediário', 'Gravou em estúdio para um álbum de jazz', 'Saxofone', 'Jazz', 'Anna Müller', 'Alemanha'),
+(1005, 'Tóquio', 'takashi@example.com', 'Avançado', 'Tocou em uma orquestra por 10 anos', 'Violino', 'Clássico', 'Takashi Yamamoto', 'Japão'),
+(1006, 'Paris', 'emilie@example.com', 'Intermediário', 'Participou de vários festivais de jazz', 'Baixo', 'Jazz', 'Jovinalda Dupont', 'França'),
+(1007, 'Sydney', 'liam@example.com', 'Iniciante', 'Apresentou em bares e pubs locais', 'Vocal', 'Pop/Rock', 'Liam Evans', 'Austrália'),
+(1008, 'Toronto', 'emily@example.com', 'Avançado', 'Fez turnês com uma banda country', 'Banjo', 'Country', 'Emily Thompson', 'Canadá'),
+(1009, 'Moscou', 'vladimir@example.com', 'Intermediário', 'Tocou em uma orquestra sinfônica', 'Violoncelo', 'Clássico', 'Vladimir Petrov', 'Rússia'),
+(1010, 'São Paulo', 'maria@example.com', 'Iniciante', 'Participou de apresentações de talentos escolares', 'Flauta', 'Pop', 'Maria Silva', 'Brasil');
 
 --BAND
 INSERT INTO band (id, city, contact, music_genre, name, state)
 VALUES
-(1, 'Nova Iorque', 'info@banda1.com', 'Rock', 'Banda do Rock', 'Nova York'),
-(2, 'Los Angeles', 'contato@banda2.com', 'Pop', 'Banda Popstar', 'Califórnia'),
-(3, 'Londres', 'banda3@gmail.com', 'Indie', 'Indie Rockers', 'Inglaterra'),
-(4, 'Berlim', 'banda4@gmail.com', 'Eletrônica', 'Electro Beats', 'Alemanha'),
-(5, 'Tóquio', 'banda5@banda5.com', 'J-Pop', 'Tokyo Stars', 'Japão'),
-(6, 'Paris', 'contato@banda6.com', 'Chanson', 'Les Amoureux', 'França'),
-(7, 'Sydney', 'banda7@hotmail.com', 'Rock Alternativo', 'The Sydney Sound', 'Austrália'),
-(8, 'Toronto', 'contato@banda8.com', 'Country', 'Country Roads', 'Canadá'),
-(9, 'Moscou', 'banda9@gmail.com', 'Rock Clássico', 'Moscow Legends', 'Rússia'),
-(10, 'São Paulo', 'banda10@gmail.com', 'Samba', 'Sambalanço', 'Brasil');
+(1001, 'Nova Iorque', 'info@banda1.com', 'Rock', 'Banda do Rock', 'Nova York'),
+(1002, 'Los Angeles', 'contato@banda2.com', 'Pop', 'Banda Popstar', 'Califórnia'),
+(1003, 'Londres', 'banda3@gmail.com', 'Indie', 'Indie Rockers', 'Inglaterra'),
+(1004, 'Berlim', 'banda4@gmail.com', 'Eletrônica', 'Electro Beats', 'Alemanha'),
+(1005, 'Tóquio', 'banda5@banda5.com', 'J-Pop', 'Tokyo Stars', 'Japão'),
+(1006, 'Paris', 'contato@banda6.com', 'Chanson', 'Les Amoureux', 'França'),
+(1007, 'Sydney', 'banda7@hotmail.com', 'Rock Alternativo', 'The Sydney Sound', 'Austrália'),
+(1008, 'Toronto', 'contato@banda8.com', 'Country', 'Country Roads', 'Canadá'),
+(1009, 'Moscou', 'banda9@gmail.com', 'Rock Clássico', 'Moscow Legends', 'Rússia'),
+(1010, 'São Paulo', 'banda10@gmail.com', 'Samba', 'Sambalanço', 'Brasil');
 
 -- BAND_FAVORITES
 INSERT INTO band_favorites (band_id, favorites_id)
-VALUES(1,1);
+VALUES(1001,1001);
 
 --NOTIFICATION
 INSERT INTO notification (id, message, read, band_id, musician_id)
-VALUES(1, 'abc', false, 1, 1);
-
---FILE
+VALUES(1001, 'abc', false, 1001, 1001);
